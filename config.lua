aab68c41f76c4a1e0fbc7342b72042ed98fd50d9
Buzzaaaaar <harshana.kavindu1@gmail.com>
2022-09-09 23:27:51 +0930

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.charLen = 6 -- Number of digits you want in the phone number
-Config.randomnumber = true -- Set this to false if you want the players to be able to choose their own number
-
