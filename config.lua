de4cf78aa973db95cb792fdbff0202931cbb1be0
Buzzaaaaar <harshana.kavindu1@gmail.com>
2022-09-09 22:32:44 +0930

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.charLen = 6 -- Number of digits you want in the phone number
+Config.randomnumber = true -- Set this to false if you want the players to be able to choose their own number
+
